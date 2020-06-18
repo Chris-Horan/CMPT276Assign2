aa51b4edd6640a5207938654de8f6bff060c7513
Chris-Horan <chris.horan27@gmail.com>
2020-06-17 20:23:29 -0700

    changed port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.listen('port', function() {
-    console.log('listening on 5050');
+    console.log('listening on 5000');
 });
 
 app.get('/', function(req, res) {
